f42daf3e03dd02955a8ad316727fe22d94edfe66
fofosharawi <yazansharawi25@gmail.com>
2022-10-17 19:57:33 +0300

    2nd push

--- onexproject/src/App.vue
+++ onexproject/src/App.vue
@@ -1,28 +1,24 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Yazan Sharawi"/>
-  </div>
+  <div class = "MainPage">
+    <Header></Header>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './Components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+  },
 }
 </script>
 
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './App.css';
 </style>
---
+++ onexproject/src/Components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class = "app">
+    <h1>Social Media Dashboard</h1>
+    <h1>hello</h1>
+</div>
+</template>
+
+<script>
+export default{
+    name: "Header",
+    components: {},
+}
+</script>
+
+<style>
+@import './Header.css'
+</style>
